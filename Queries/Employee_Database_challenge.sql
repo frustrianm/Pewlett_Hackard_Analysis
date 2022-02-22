20a626996f79f96ec5b2e3b950fa5e9658920061
Florencio Rustrián Monroy <96660344+frustrianm@users.noreply.github.com>
2022-02-21 21:38:22 -0600

    Deliverable 1 and 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -40,10 +40,23 @@ ORDER BY _____, _____ DESC;
 
 
 -- Deliverable 2: The Employees Eligible for the Mentorship Program
-
-
-
-
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_elegibility
+FROM employees as e
+INNER JOIN deptemp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no ASC;
 
 -- END OF CHALLENGE 7 CODE
--- Florencio Rustrian Monroy
+-- Florencio Rustrian Monroy 
+-- I loved the PostgreSQL module, I'd like to get deeper in it, any course of action recommended?
